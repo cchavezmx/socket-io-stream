e60037a56d5304e208662f17a8286c6a0b370164
cchavezmx <cchavezmx@outlook.com>
2023-04-24 03:13:05 -0600

    test another server conf

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ import { Server } from "socket.io";
 import path from 'path';
 import cors from 'cors';
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 const app = express();
 const server = createServer(app)
 const io = new Server(server, {
