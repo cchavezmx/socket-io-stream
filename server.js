baf108e4249a96660d7415e029e094c856196c8b
cchavezmx <cchavezmx@outlook.com>
2023-04-24 03:04:19 -0600

    test another server conf

--- server.js
+++ server.js
@@ -1,34 +1,47 @@
-import path from 'node:path'
-import express from 'express'
-import { Server } from 'socket.io'
-import cors from 'cors'
-import http from 'http'
+import express from 'express';
+import { createServer } from 'http';
+import { Server } from "socket.io";
+import path from 'path';
+import cors from 'cors';
 
-const app = express()
-const __dirname = path.resolve()
+const port = 3000;
+const app = express();
+const server = createServer(app)
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:5173",
+  }
+});
 
-const server = http.createServer(app)
-const io = new Server(server)
+const __dirname = path.resolve();
 
-const PORT = process.env.PORT || 3000
-const HTML_FILE = path.join(__dirname, 'index.html')
+app.use(cors());
 
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+})
 
-app.use(cors())
-app.use(express.static(HTML_FILE))
 
 io.on('connection', (socket) => {
   console.log('a user connected')
+
+  socket.on('howdy', (msg) => {
+    console.log(msg)    
+    
+  })
+
+  
+  socket.on('chat message', (msg) => {
+    socket.broadcast.emit('chat message', msg)
+  })
+
   socket.on('disconnect', () => {
     console.log('user disconnected')
   })
 })
 
-app.get('*', (req, res) => {
-  res.sendFile(HTML_FILE)
+server.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 })
 
-app.listen(PORT, () => {
-  console.log(`App listening to ${PORT}....`)
-  console.log('Press Ctrl+C to quit.')
-})
+
